4aca9400a6536f26655caf22b014379b343ac127
L。 <421558569@qq.com>
2023-12-13 16:05:14 +0800

    [84]柱状图中最大的矩形，Monotone Stack解法

--- 84.Largest-Rectangle-in-Histogram/main.go
+++ 84.Largest-Rectangle-in-Histogram/main.go
@@ -4,8 +4,49 @@ func main() {
 
 	//println(largestRectangleArea([]int{2, 1, 5, 6, 2, 3}))
 	//println(largestRectangleArea([]int{2, 4}))
+	//println(largestRectangleArea([]int{1}))
+
+	println(largestRectangleAreaStack([]int{2, 1, 5, 6, 2, 3}))
+	println(largestRectangleArea([]int{2, 4}))
 	println(largestRectangleArea([]int{1}))
 
+}
+
+// 使用单调递增栈，碰到递减的元素就停下来计算面积
+// 意思就是说：只要是单调递增，就能以任意一边为高度，一直向右滑到底，来计算面积
+// 因为右边的值比你大，肯定能包括你
+func largestRectangleAreaStack(heights []int) int {
+	res := 0
+	stack := []int{}
+	// 为了让最后一个元素入栈，所以多添加了一个为面积为0的柱子
+	heights = append(heights, 0)
+
+	size := len(heights)
+	for i := 0; i < size; i++ {
+		if len(stack) == 0 || heights[stack[len(stack)-1]] < heights[i] {
+			stack = append(stack, i)
+		} else {
+			index := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			height := heights[index]
+			length := func() int {
+				if len(stack) == 0 {
+					return i
+				}
+				// 为什么是需要-1？
+				// 我们以[2, 1, 5, 6, 2, 3]为例
+				// 当i=4时，此时stack为[2,5]，因为6在上面已经出栈了，而i代表的位置又在6的后面，所以要-1
+				// 此时算出来的面积是柱子6的单个面积
+				return i - stack[len(stack)-1] - 1
+			}
+			res = max(res, height*length())
+			// 因为我们只是在stack里面自己玩，并没有处理heights[i]
+			// 所以要回去重新处理heights[i]
+			i--
+		}
+	}
+
+	return res
 }
 
 // 最后几个case超时了
