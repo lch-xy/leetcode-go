e88f9877e1f3b57e92697b475bce5c13a4c336ef
L。 <421558569@qq.com>
2023-12-17 18:43:56 +0800

    [97]交错字符串 递归解法

--- 0097.Interleaving-String/main.go
+++ 0097.Interleaving-String/main.go
@@ -1,10 +1,49 @@
 package main
 
+import "strconv"
+
 func main() {
 
 	println(isInterleave("qwertyuiop", "asdfghjkl", "qawsedrftyughjikolp"))
 	println(isInterleave("a", "", "a"))
-	
+
+	println("================================================================>")
+
+	println(isInterleaveRecursion("qwertyuiop", "asdfghjkl", "qawsedrftyughjikolp"))
+	println(isInterleaveRecursion("a", "", "a"))
+
+}
+
+func isInterleaveRecursion(s1 string, s2 string, s3 string) bool {
+	if len(s1)+len(s2) != len(s3) {
+		return false
+	}
+	cache := map[string]string{}
+	return helper(s1, 0, s2, 0, s3, 0, cache)
+}
+
+func helper(s1 string, i int, s2 string, j int, s3 string, k int, cache map[string]string) bool {
+	// 将i和j的关系映射到key里就可以，随便什么方法都行
+	key := strconv.Itoa(i*len(s3) + j)
+	_, ok := cache[key]
+	if ok {
+		return false
+	}
+	if i == len(s1) {
+		// 如果匹配上就返回true 匹配不是就返回false
+		return s2[j:] == s3[k:]
+	}
+	if j == len(s2) {
+		return s1[i:] == s3[k:]
+	}
+	// 看s3的当前位置能都和s1、s2的任意位置匹配上
+	if (s1[i] == s3[k] && helper(s1, i+1, s2, j, s3, k+1, cache)) ||
+		(s2[j] == s3[k] && helper(s1, i, s2, j+1, s3, k+1, cache)) {
+		return true
+	}
+	// 如果没匹配上 下次遇到直接返回
+	cache[key] = key
+	return false
 }
 
 // dp[i][j] 以s1[:i-1]和s2[:j-1]能否组成s3[:i+j]
