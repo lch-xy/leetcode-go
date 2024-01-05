9caabd0564b4a17d03313ff8ce78e54a475485b2
L。 <421558569@qq.com>
2024-01-05 10:50:22 +0800

    [685]冗余连接 II 添加解题思路

--- 0685.Redundant-Connection-II/main.go
+++ 0685.Redundant-Connection-II/main.go
@@ -22,6 +22,10 @@ func main() {
 			edges: [][]int{{2, 1}, {3, 1}, {4, 2}, {1, 4}},
 			want:  []int{2, 1},
 		},
+		{
+			edges: [][]int{{2, 1}, {3, 1}, {1, 4}, {4, 2}},
+			want:  []int{2, 1},
+		},
 	}
 
 	for _, tt := range tests {
@@ -33,6 +37,10 @@ func main() {
 }
 
 // 并查集
+// 为什么 UnionFind 里已经有 ancestor 数组了还需要一个 parent 数组？
+// ancestor 放的是祖先节点，是为了更快的做 union 操作
+// parent 主要是处理一个节点入度为 2 的情况，如果我们发现一个节点的父节点已经存在（即 parent[node2] != node2）
+// 说明这个节点已经有一个父节点了，如果现在又有一个新的节点想成为这个节点的父节点，这就产生了冲突。就把 index 保存到 conflict
 func findRedundantDirectedConnection(edges [][]int) []int {
 	size := len(edges)
 	uf := initUnionFind(size)
@@ -51,6 +59,7 @@ func findRedundantDirectedConnection(edges [][]int) []int {
 		} else {
 			parent[node2] = node1
 			// 这两个节点如果根节点相同，那么再连接这两个节点就会成环
+			// 如果成环则记录成环的 index 下标，如果正常就进行 union 操作
 			if uf.find(node1) == uf.find(node2) {
 				cycle = i
 			} else {
@@ -65,8 +74,12 @@ func findRedundantDirectedConnection(edges [][]int) []int {
 	} else {
 		conflictEdge := edges[conflict]
 		if cycle > 0 {
-			// 因为如果发现冲突就不会执行 union 操作，不然就成环了
-			// 所以返回的是冲突的边和它的父节点
+			// 以[[2,1],[3,1],[4,2],[1,4]]为例
+			// 解释如果出现 conflict 和 cycle 都大于 0 的情况
+			// 我们发现[3,1]是冲突的边，[1,4]是成环的边
+			// 所以我们无论删除[3,1]还是[1,4]都是没用的
+			// 我们通过冲突的边可以找到[节点1]的另一个父亲[节点2]，我们是通过parent[node2] = node1 进行赋值的
+			// 我们只需要将这条边删掉即可
 			return []int{parent[conflictEdge[1]], conflictEdge[1]}
 		} else {
 			// 冲突没有成环，返回当前这条边
