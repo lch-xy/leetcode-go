ec449bf0ee7bd82a66dffcf7647c0bf8ae954084
L。 <421558569@qq.com>
2024-03-13 22:19:08 +0800

    [217]存在重复元素

---
+++ 0217.Contains-Duplicate/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "sort"
+
+func containsDuplicate(nums []int) bool {
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] == nums[i] {
+			return true
+		}
+	}
+	return false
+}
